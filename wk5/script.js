f5a1ab3647e85e22fbc4a4891286163c079c41d5
rastirland <rastirland@hotmail.com>
2023-03-10 15:54:40 +0000

    struggling on paragraph

--- wk5/script.js
+++ wk5/script.js
@@ -10,13 +10,37 @@ function toggleTransparent() {
     const circle = document.getElementById('circle');
 
     //activates when hoover over click me
-    circle.addEventListener('mouseover', toggleTransparent);
-    circle.addEventListener('mouseout', toggleTransparent);
+    // circle.addEventListener('mouseover', toggleTransparent);
+    // circle.addEventListener('mouseout', toggleTransparent);
     // activates when clicked 
-    // circle.addEventListener('click', toggleTransparent);
+    circle.addEventListener('click', toggleTransparent);
   }
-  
   document.addEventListener('DOMContentLoaded', myLoadFunction);
+
+// function toggleTransparent() {
+//     const circle = document.getElementById('circle');
+//     circle.classList.toggle('transparent');
+//   }
+  
+//   function toggleP() {
+//     const p = document.getElementById('square');
+//     square.classList.toggle('transparent1');
+//   }
+  
+//   function myLoadFunction() {
+//     const circle = document.getElementById('circle');
+//     const square = document.getElementById('square');
+  
+//     circle.addEventListener('click', toggleTransparent);
+//     p.addEventListener('click', toggleP);
+//   }
+  
+//   document.addEventListener('DOMContentLoaded', myLoadFunction);
+
+
+
+
+
 
 // modifying this to const circle, and using the myload function, used this as abasis for my solution. 
 //     function clickFunction() {
